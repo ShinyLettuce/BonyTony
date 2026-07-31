444c0dc05ea16b800e727eb42f22b1178f4cdf5d
ShinyLettuce <je03@live.se>
2026-07-31 17:29:24 +0200

    Options 100

--- Source/Game/source/Options.h
+++ Source/Game/source/Options.h
@@ -3,8 +3,8 @@
 namespace Options
 {
 
-	inline int masterVolume = 33;
-	inline int musicVolume = 33;
+	inline int masterVolume = 100;
+	inline int musicVolume = 100;
 	inline int minVolume = 0;
 	inline int maxVolume = 100;
 
